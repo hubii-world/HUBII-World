bd8162e8ca53593fa0bae0894f2e544db7905011
wj5601 <elias.mueller@kit.edu>
2025-04-09 18:30:34 +0200

    add output path

--- sample_HUBIIPipe.py
+++ sample_HUBIIPipe.py
@@ -1,17 +1,20 @@
 from transformers import pipeline
 import pandas as pd
 
+input_file_path = "./Example_data/TestData_2.csv"
+output_file_path = "./output.csv"
+
 print("load pipe")
 rr2hrv = pipeline(model = "hubii-world/rpeaks-to-hrv-pipeline", trust_remote_code=True)
 
-file_path = "./Example_data/TestData_2.csv"
 
 print("calculate data")
 ## Calculate HRV for the full file
-result = rr2hrv(inputs=file_path, sampling_rate=130)
+result = rr2hrv(inputs=input_file_path, sampling_rate=130)
+result.to_csv(output_file_path)
 print(result.head())
 
-
 ## Calculate HRV with rolling windows
-result = rr2hrv(inputs=file_path, window_method='rolling',window_size="5m", sampling_rate=130)
+result = rr2hrv(inputs=input_file_path, window_method='rolling',window_size="5m", sampling_rate=130)
+result.to_csv(output_file_path)
 print(result.head())
