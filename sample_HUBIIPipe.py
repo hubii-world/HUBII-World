cf1dc7b49ebe15f93c411355ba2a1b244d7029b2
wj5601 <elias.mueller@kit.edu>
2025-04-09 18:23:41 +0200

    change sample hubii rec

--- sample_HUBIIPipe.py
+++ sample_HUBIIPipe.py
@@ -1,14 +1,17 @@
 from transformers import pipeline
+import pandas as pd
 
+print("load pipe")
 rr2hrv = pipeline(model = "hubii-world/rpeaks-to-hrv-pipeline", trust_remote_code=True)
 
-file_path = "./Example_data/RRIntervalExample.csv"
+file_path = "./Example_data/TestData_2.csv"
 
+print("calculate data")
 ## Calculate HRV for the full file
-result = rr2hrv(inputs=file_path, sampling_rate=1024)
-result.head()
+result = rr2hrv(inputs=file_path, sampling_rate=130)
+print(result.head())
 
 
 ## Calculate HRV with rolling windows
 result = rr2hrv(inputs=file_path, window_method='rolling',window_size="5m", sampling_rate=130)
-result.head()
+print(result.head())
